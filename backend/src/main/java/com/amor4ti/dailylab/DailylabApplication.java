0e24bb977679a720355cb03f43269cd4a658aa42
Junyoung <wnsdud12365@naver.com>
2023-09-10 17:31:36 +0900

    FEAT: BE 프로젝트 생성

---
+++ backend/src/main/java/com/amor4ti/dailylab/DailylabApplication.java
@@ -0,0 +1,13 @@
+package com.amor4ti.dailylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailylabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailylabApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/amor4ti/dailylab/DailylabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amor4ti.dailylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailylabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
