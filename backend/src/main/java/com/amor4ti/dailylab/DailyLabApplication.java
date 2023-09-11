5d4bd4559afc76167e506d2664d1d9f221672705
ParkSeYun98 <yun12343@naver.com>
2023-09-11 09:50:17 +0900

    STYLE: Class 이름 변경

--- backend/src/main/java/com/amor4ti/dailylab/DailyLabApplication.java
+++ backend/src/main/java/com/amor4ti/dailylab/DailyLabApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DailylabApplication {
+public class DailyLabApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DailylabApplication.class, args);
+		SpringApplication.run(DailyLabApplication.class, args);
 	}
 
 }
