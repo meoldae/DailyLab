a5014a2fdba1733704b71c6f799b0f048d603957
정민 <k3371548@naver.com>
2023-09-11 13:19:18 +0900

    [FEAT] 0911 JM font-styld add

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import AppRouter from "@/router/AppRouter";
 import { RecoilRoot } from 'recoil';
+import "@/resources/css/common/import.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <RecoilRoot>
--- frontend/src/api/User.ts
+++ frontend/src/api/User.ts
@@ -4,8 +4,8 @@ interface props {
     data : string
 }
 
-const RequestTest =async (param: object, success: (data : {data : props}) => void, fail: (error: unknown) => void) => {
+const Login =async (param: object, success: (data : {data : props}) => void, fail: (error: unknown) => void) => {
     await HttpJson.post(`/diary`, JSON.stringify(param)).then(success).catch(fail);
 }
 
-export { RequestTest };
+export { Login };
