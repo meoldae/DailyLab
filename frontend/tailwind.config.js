22ca94505e7eee441c14d7de83cd64174c59b472
정민 <k3371548@naver.com>
2023-09-11 15:19:33 +0900

    [FEAT] 0911 JM PWA setting add

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens : {
+      sm : '768px',
+      md : '992px',
+      lg : '1200px',
+      xl : '1600px',
+    }
   },
   variants: {
     extend: {},
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions : {
+        enabled: true,
+      },
+      manifest: {
+        icons: [
+          {
+            "src": "icons/coco_icon.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any maskable"
+          }
+        ],
+      },
+  }),
+  ],
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
