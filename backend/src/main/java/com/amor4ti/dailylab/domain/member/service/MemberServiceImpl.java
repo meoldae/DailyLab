8f2f648567e1949d585782d0413fa1f1944523c4
Junyoung <wnsdud12365@naver.com>
2023-09-18 22:39:05 +0900

    FEAT: Member Update 구현 (Hobby List 제외)

--- backend/src/main/java/com/amor4ti/dailylab/domain/member/service/MemberServiceImpl.java
+++ backend/src/main/java/com/amor4ti/dailylab/domain/member/service/MemberServiceImpl.java
@@ -1,25 +1,26 @@
 package com.amor4ti.dailylab.domain.member.service;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
-
-import com.amor4ti.dailylab.domain.member.dto.MainMemberDto;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.amor4ti.dailylab.domain.entity.Member;
+import com.amor4ti.dailylab.domain.hobby.service.MemberHobbyService;
+import com.amor4ti.dailylab.domain.member.dto.MainMemberDto;
 import com.amor4ti.dailylab.domain.member.dto.SignUpDto;
+import com.amor4ti.dailylab.domain.member.dto.UpdateMemberDto;
 import com.amor4ti.dailylab.domain.member.repository.MemberRepository;
 import com.amor4ti.dailylab.global.exception.CustomException;
 import com.amor4ti.dailylab.global.exception.ExceptionStatus;
+import com.amor4ti.dailylab.global.response.CommonResponse;
 import com.amor4ti.dailylab.global.response.DataResponse;
 import com.amor4ti.dailylab.global.response.ResponseService;
 import com.amor4ti.dailylab.global.response.ResponseStatus;
 import com.amor4ti.dailylab.global.util.CookieUtils;
 import com.amor4ti.dailylab.global.util.JwtProvider;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @Service
@@ -30,6 +31,7 @@ public class MemberServiceImpl implements MemberService{
 	private final JwtProvider jwtProvider;
 	private final CookieUtils cookieUtils;
 	private final ResponseService responseService;
+	private final MemberHobbyService memberHobbyService;
 
 	@Override
 	@Transactional
@@ -58,5 +60,21 @@ public class MemberServiceImpl implements MemberService{
 				() -> new CustomException(ExceptionStatus.MEMBER_NOT_FOUND)
 		);
 		return responseService.successDataResponse(ResponseStatus.RESPONSE_SUCCESS, mainMemberDto);
+	}
+
+	@Transactional
+	@Override
+	public CommonResponse updateMemberInfo(Long memberId, UpdateMemberDto updateMemberDto) {
+		Member findMember = memberRepository.findById(memberId).orElseThrow(
+				() -> new CustomException(ExceptionStatus.MEMBER_NOT_FOUND)
+		);
+
+		findMember.updateMember(updateMemberDto);
+		// Dirty Checking 이상으로 Save 호출
+		memberRepository.save(findMember);
+		
+		// TODO Hobby List 반영 필요
+		
+		return responseService.successResponse(ResponseStatus.RESPONSE_SUCCESS);
 	}
 }
