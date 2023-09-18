c4acc75a8619e2d7085887cd54dab3804774c8f4
Junyoung <wnsdud12365@naver.com>
2023-09-18 16:46:34 +0900

    FEAT: 액세스토큰, 리프레시토큰 상태코드 추가

--- backend/src/main/java/com/amor4ti/dailylab/global/response/ResponseStatus.java
+++ backend/src/main/java/com/amor4ti/dailylab/global/response/ResponseStatus.java
@@ -11,10 +11,9 @@ public enum ResponseStatus {
 
     ACCESS_TOKEN_REFRESHED("1001", "액세스 토큰이 재 발급되었습니다"),
 
-    SIGNUP_SUCCESS("2000", "회원 가입이 완료 되었습니다.");
-
-
-
+    SIGNUP_SUCCESS("2000", "회원 가입이 완료 되었습니다."),
+    LOGIN_SUCCESS("2001", "로그인 되었습니다."),
+    LOGOUT_SUCCESS("2002", "로그아웃 되었습니다.");
 
     private final String code;
     private final String message;
