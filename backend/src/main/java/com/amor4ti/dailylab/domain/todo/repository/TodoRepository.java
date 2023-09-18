a27aa73d3a57dd85602ed5fed84900f014859d80
박세윤 <yun12343@naver.com>
2023-09-19 02:16:19 +0900

    STYLE: JoinColumn 이름 원상복구

--- backend/src/main/java/com/amor4ti/dailylab/domain/todo/repository/TodoRepository.java
+++ backend/src/main/java/com/amor4ti/dailylab/domain/todo/repository/TodoRepository.java
@@ -2,6 +2,7 @@ package com.amor4ti.dailylab.domain.todo.repository;
 
 import com.amor4ti.dailylab.domain.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,6 @@ import java.util.List;
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
+    @Query("SELECT t FROM Todo t WHERE t.member.memberId = :memberId")
     List<Todo> findByMemberId(Long memberId);
 }
