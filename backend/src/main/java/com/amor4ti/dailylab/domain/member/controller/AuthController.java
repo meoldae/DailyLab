a44f7270541fcd464ccd92b9032697f77e6dc109
Junyoung <wnsdud12365@naver.com>
2023-09-18 14:08:09 +0900

    FEAT: 토큰 Refresh 기능 구현

---
+++ backend/src/main/java/com/amor4ti/dailylab/domain/member/controller/AuthController.java
@@ -0,0 +1,43 @@
+package com.amor4ti.dailylab.domain.member.controller;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.amor4ti.dailylab.domain.member.service.AuthService;
+import com.amor4ti.dailylab.global.exception.CustomException;
+import com.amor4ti.dailylab.global.exception.ExceptionStatus;
+import com.amor4ti.dailylab.global.response.DataResponse;
+import com.amor4ti.dailylab.global.util.CookieUtils;
+import com.amor4ti.dailylab.global.util.JwtProvider;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+	private final JwtProvider jwtProvider;
+	private final CookieUtils cookieUtils;
+	private final AuthService authService;
+	private static final String REFRESH_TOKEN = "refreshToken";
+
+	@PostMapping("/refresh")
+	public DataResponse tokenRefresh(HttpServletRequest request){
+		String accessToken = jwtProvider.getAccessToken(request);
+		Cookie cookie = cookieUtils.getCookie(request, REFRESH_TOKEN).orElseThrow(
+			() -> new CustomException(ExceptionStatus.TOKEN_NOT_FOUND_IN_COOKIE)
+		);
+
+		return authService.refresh(accessToken, cookie.getValue());
+	}
+
+
+
+}
