f8a2880feb8dd48763a8f6f45e44b1d68f05e2c4
김주하 <tkfkd122@naver.com>
2023-09-20 04:25:03 +0900

    FEAT: CBF 기반 추천 함수 및 테스트 함수 작성

---
+++ data/domain/todo/contents_based_filtering/cbf.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+filePath = '../../../dataset/'
+cosine_similarity_matrix = pd.read_csv(filePath + 'cosine_similarity_matrix.csv', encoding='utf-8')
+
+def printSim(A):
+    return cosine_similarity_matrix[A]
+
+
+def findDummy():
+    firstList = [88, 3]
+    topFiveRecords = [4, 18, 22, 90, 220]
+    resultList = [0] * 290
+    for record in topFiveRecords:
+        resultList = resultList + printSim(str(record))
+
+    for item in firstList:
+        print(item)
+        resultList = resultList.drop(item)
+
+    resultList = resultList.sort_values(ascending=False)
+    return resultList
+
+print(findDummy())
