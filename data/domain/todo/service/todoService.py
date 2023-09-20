c458706902cf9df0488da449af19f570f2c9c5a1
김주하 <tkfkd122@naver.com>
2023-09-21 00:23:43 +0900

    FEAT: 서비스 로직 변경 및 에러 수정

--- data/domain/todo/service/todoService.py
+++ data/domain/todo/service/todoService.py
@@ -1,5 +1,5 @@
 from domain.todo.repository import todoRepository
-from sqlalchemy import desc
+from sqlalchemy import desc, func
 
 from mysql import models
 from domain.todo.contents_based_filtering import cbf
@@ -10,19 +10,27 @@ def makeTodo(member_id: int, db):
     firstList = todoRepository.getUserTodo(member_id, db)
 
     # 최근 7일치 중 가장 많이 등록된 category를 5개만 가져옴
-    topFiveRecords = todoRepository.getRecomendedList(member_id, db)
-    topFiveRecords = topFiveRecords.order_by(desc(models.todo_report.success_count + models.todo_report.fail_count))
-    topFiveRecords = topFiveRecords.limit(5).all()
+    topFiveRecords = todoRepository.getRecommendedList(member_id, db)
+    # topFiveRecords 리스트를 fail_count와 success_count의 합을 기준으로 내림차순 정렬
+    topFiveRecords = sorted(topFiveRecords, key=lambda x: x.fail_count + x.success_count, reverse=True)
+    # 상위 5개 레코드 선택
+    topFiveRecords = topFiveRecords[:5]
 
     # 선언
     resultList = [0] * 290
-    for record in topFiveRecords:
-        category_id = record.category_id
-        resultList = resultList + cbf.printSim(str(category_id))
+    if topFiveRecords:
+        for record in topFiveRecords:
+            category_id = record.category_id
+            resultList = resultList + cbf.printSim(str(category_id))
+    else:
+        topFiveRecords = [4, 18, 22, 90, 220]
+        for record in topFiveRecords:
+            resultList = resultList + cbf.printSim(str(record))
 
-    for remove in firstList:
-        category_id = remove.category_id
-        resultList = resultList.drop(category_id)
+    if firstList :
+        for remove in firstList:
+            category_id = remove.category_id
+            resultList = resultList.drop(category_id)
 
     resultList = resultList.sort_values(ascending=False)
 
