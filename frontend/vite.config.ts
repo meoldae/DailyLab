3429aa6b2996cdc86cc3ddc323f2994975ee46dd
JM <k3371548@gmail.com>
2023-09-21 00:08:41 +0900

    FIX: 0920 JM service-worker edit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      injectRegister: 'auto',
+      injectRegister: null,
       devOptions : {
         enabled: true,
       },
