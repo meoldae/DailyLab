759271ce8abdb6133697a20d5e5d6e912f1ae9dd
김주하 <tkfkd122@naver.com>
2023-09-21 00:23:15 +0900

    FEAT: post request schema 추가

--- data/mysql/schemas.py
+++ data/mysql/schemas.py
@@ -1,7 +1,8 @@
-import datetime
+from datetime import datetime
 from typing import Optional
 
 from pydantic import BaseModel
 
-class getTodo(BaseModel):
+class todoReq(BaseModel):
     id: int
+    date: datetime
