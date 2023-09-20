32c779cbc7a425a55548f1284d113f2e30897c5d
정민 <k3371548@naver.com>
2023-09-20 13:35:13 +0900

    FEAT: 0920 JM service worker commit

--- frontend/serviceWorker.js
+++ frontend/serviceWorker.js
@@ -38,6 +38,7 @@ self.addEventListener('fetch', event => {
 
   // Directly fetch the request if it includes /img/404error.jpg or if it's an API request
   if (checkurl.includes('/api') && checkurl.includes('/manifest.json')) {
+    console.log("____________________________test____________________________")
     event.respondWith(fetch(event.request));
     return;
   }
