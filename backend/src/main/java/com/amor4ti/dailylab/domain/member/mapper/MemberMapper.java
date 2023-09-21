4dc704071f9eee4bff9ef211c2f742753b994f01
김주하 <tkfkd122@naver.com>
2023-09-21 20:13:16 +0900

    FEAT: memberMapper 추가

---
+++ backend/src/main/java/com/amor4ti/dailylab/domain/member/mapper/MemberMapper.java
@@ -0,0 +1,16 @@
+package com.amor4ti.dailylab.domain.member.mapper;
+
+import com.amor4ti.dailylab.domain.entity.Hobby;
+import com.amor4ti.dailylab.domain.entity.Member;
+import com.amor4ti.dailylab.domain.member.dto.MainMemberDto;
+import com.amor4ti.dailylab.domain.member.dto.UpdateMemberDto;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface MemberMapper {
+
+    UpdateMemberDto memberToUpdateMember(Member member, List<Hobby> hobbyList);
+    MainMemberDto memberToMainMember(Member member);
+}
