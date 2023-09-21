a9f20cdb0605ff03e0539cedba145b6d33b3b686
97Kzone <76652908+97Kzone@users.noreply.github.com>
2023-09-21 21:29:29 +0900

    FEAT: Member Status Entity 추가

---
+++ backend/src/main/java/com/amor4ti/dailylab/domain/entity/MemberStatus.java
@@ -0,0 +1,24 @@
+package com.amor4ti.dailylab.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberStatus {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberStatusId;
+
+    private Long memberId;
+    private LocalDate date;
+    private String status;
+}
