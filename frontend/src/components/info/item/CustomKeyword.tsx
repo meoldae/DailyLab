084cc44c7fc5bdbd0f99d06ce457682e0541443b
정민 <k3371548@naver.com>
2023-09-22 12:54:18 +0900

    FEAT: 0922 JM logout, keyword active commit

--- frontend/src/components/info/item/CustomKeyword.tsx
+++ frontend/src/components/info/item/CustomKeyword.tsx
@@ -8,7 +8,7 @@ interface props {
 
 const CustomKeyword = (data: props) => {
     return (
-        <div onClick={() => data.clickEvent(data.activeStatus, data.idx)} className={ (data.activeStatus ? "bg-reverse-primary text-primary" : "bg-secondary") + ` rounded-[10px] px-[10px] py-[6px] text-[13px] inline-flex justify-between items-center mr-[11px] mb-[7px] transition cursor-pointer hover:bg-reverse-primary hover:text-primary`}>
+        <div onClick={() => data.clickEvent(data.activeStatus, data.idx)} className={ (data.activeStatus ? "bg-reverse-primary text-primary" : "bg-secondary") + ` rounded-[10px] px-[10px] py-[6px] text-[13px] inline-flex justify-between items-center mr-[11px] mb-[7px] transition cursor-pointer action:bg-reverse-primary action:text-primary`}>
             <span>{data.name}</span> {data.XStatus ? <span className="inline-block ml-[10px]">X</span> : null}
         </div>
     )
