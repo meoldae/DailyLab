c4ccc5b00af7290145c4f4cb0b7cc965852ba804
정민 <k3371548@naver.com>
2023-09-22 12:59:48 +0900

    FIX: 0922 JM production environment test commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,11 @@ import './tailwind.css';
 import "@/styles/common/fontImport.css";
 import { register } from './serviceWorkerRegistration';
 
+const test = import.meta.env.PROD;
+const test2 = import.meta.env.DEV;
+console.log("프로덕션환경 : " + test);
+console.log("개발환경 : " + test2);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <RecoilRoot>
     <AppRouter />
