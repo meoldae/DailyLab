7ba4a439cdd1ea36d295b4b40992891eac95b56f
Junyoung <wnsdud12365@naver.com>
2023-09-24 17:08:01 +0900

    FEAT: 서비스워커 JS 파일 경로 변경

--- frontend/src/serviceWorkerRegistration.ts
+++ frontend/src/serviceWorkerRegistration.ts
@@ -35,7 +35,8 @@ export function register(config?: Config) {
     }
 
     window.addEventListener("load", () => {
-      const swUrl = `${import.meta.env.BASE_URL}service-worker.js`;
+      // const swUrl = `${import.meta.env.BASE_URL}service-worker.js`;
+      const swUrl = "/sw.js";
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
