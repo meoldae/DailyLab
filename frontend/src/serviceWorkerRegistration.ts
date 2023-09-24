e4eb2f140aab2746feeaeedb455a24088c955b9a
97Kzone <76652908+97Kzone@users.noreply.github.com>
2023-09-24 17:49:17 +0900

    FIX: vite.config.ts 수정

--- frontend/src/serviceWorkerRegistration.ts
+++ frontend/src/serviceWorkerRegistration.ts
@@ -35,8 +35,8 @@ export function register(config?: Config) {
     }
 
     window.addEventListener("load", () => {
-      // const swUrl = `${import.meta.env.BASE_URL}service-worker.js`;
-      const swUrl = "https://j9b104.p.ssafy.io/service-worker.js";
+      const swUrl = `${import.meta.env.BASE_URL}service-worker.js`;
+      // const swUrl = "https://j9b104.p.ssafy.io/service-worker.js";
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
