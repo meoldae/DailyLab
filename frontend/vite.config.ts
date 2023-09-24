057d0b0781b1db07dd38501ed5ed9fccfe34626b
97Kzone <76652908+97Kzone@users.noreply.github.com>
2023-09-24 22:27:54 +0900

    FIX: vite.config.ts 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,33 +7,33 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      // swSrc: 'public/service-worker.js', 
-      // srcDir: '/',
-      // filename: 'service-worker.js',
-      // registerType: "autoUpdate",
-      // injectRegister: null,
-      // workbox : {
-      //   swDest: "service-worker.js"
-      // },
-      devOptions: {
-        enabled: true,
-        type: "module",
-      },
-      injectManifest: {
-        injectionPoint: undefined
-      },
-      manifest: {
-        icons: [
-          {
-            src: "icons/coco_icon.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
-      },
-    }),
+    // VitePWA({
+    //   swSrc: 'public/service-worker.js', 
+    //   srcDir: '/',
+    //   filename: 'service-worker.js',
+    //   registerType: "autoUpdate",
+    //   injectRegister: null,
+    //   workbox : {
+    //     swDest: "service-worker.js"
+    //   },
+    //   devOptions: {
+    //     enabled: true,
+    //     type: "module",
+    //   },
+    //   injectManifest: {
+    //     injectionPoint: undefined
+    //   },
+    //   manifest: {
+    //     icons: [
+    //       {
+    //         src: "icons/coco_icon.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //         purpose: "any maskable",
+    //       },
+    //     ],
+    //   },
+    // }),
   ],
   resolve: {
     alias: [
