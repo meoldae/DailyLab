8d607c3fc5a450958e74b5f470eb7835d5bde331
97Kzone <76652908+97Kzone@users.noreply.github.com>
2023-09-24 22:55:14 +0900

    FIX: service worker 전략 변경

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,33 +7,39 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [
     react(),
-    // VitePWA({
-    //   swSrc: 'public/service-worker.js', 
-    //   srcDir: '/',
-    //   filename: 'service-worker.js',
-    //   registerType: "autoUpdate",
-    //   injectRegister: null,
-    //   workbox : {
-    //     swDest: "service-worker.js"
-    //   },
-    //   devOptions: {
-    //     enabled: true,
-    //     type: "module",
-    //   },
-    //   injectManifest: {
-    //     injectionPoint: undefined
-    //   },
-    //   manifest: {
-    //     icons: [
-    //       {
-    //         src: "icons/coco_icon.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //         purpose: "any maskable",
-    //       },
-    //     ],
-    //   },
-    // }),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: null,
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => {
+              return url.pathname.includes("/oauth2");
+            },
+            handler: "NetworkFirst" as const,
+            options: {
+              cacheName: "oauth2-cache",
+            },
+          }]
+      },
+      devOptions: {
+        enabled: true,
+        type: "module",
+      },
+      injectManifest: {
+        injectionPoint: undefined
+      },
+      manifest: {
+        icons: [
+          {
+            src: "icons/coco_icon.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: [
@@ -44,12 +50,12 @@ export default defineConfig({
   define: {
     global: "window",
   },
-   build: {
-    rollupOptions: {
-      input: {
-        main: "./index.html",
-        sw: "public/service-worker.js",
-      },
-    },
-  },
+  //  build: {
+  //   rollupOptions: {
+  //     input: {
+  //       main: "./index.html",
+  //       sw: "public/service-worker.js",
+  //     },
+  //   },
+  // },
 });
