4e1050aeb3e04ee83d23a5142aa7a27c50b54e72
Junyoung <wnsdud12365@naver.com>
2023-09-24 19:45:25 +0900

    BUILD: 서비스워커 설정 변경

--- frontend/src/serviceWorkerRegistration.ts
+++ frontend/src/serviceWorkerRegistration.ts
@@ -35,8 +35,8 @@ export function register(config?: Config) {
     }
 
     window.addEventListener("load", () => {
-      const swUrl = `${import.meta.env.BASE_URL}service-worker.js`;
-      // const swUrl = "https://j9b104.p.ssafy.io/service-worker.js";
+      // const swUrl = `${import.meta.env.BASE_URL}service-worker.js`;
+      const swUrl = "https://j9b104.p.ssafy.io/service-worker.js";
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
@@ -60,6 +60,7 @@ export function register(config?: Config) {
 
 function registerValidSW(swUrl: string, config?: Config) {
   console.log("registerValidSW start!!");
+  console.log("sw URL : ", swUrl);
   navigator.serviceWorker
     .register(swUrl)
     .then(registration => {
