5422feaed571f877ea3f543e8fa8abd5e4f50bc5
김주하 <tkfkd122@naver.com>
2023-09-26 16:09:14 +0900

    FEAT: 유저 기반 벡터 테이블 생성, 유사도 계산, 비슷한 유저 리스트 출력 기능 추가

---
+++ data/domain/member/filtering/filtering.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+
+# filePath = '../../../dataset/'
+filePath = 'dataset/'
+cosine_similarity_matrix = pd.read_csv(filePath + 'userCosineSimilarity.csv', encoding='utf-8')
+
+def makeMatrix(matrix):
+    data = []
+    for value in matrix:
+        row_data = [
+            value.get("memberId", None),
+            value.get("ageGroup", None),
+            value.get("gender", None),
+            value.get("mbtiA", None),
+            value.get("mbtiB", None),
+            value.get("mbtiC", None),
+            value.get("mbtiD", None),
+            value.get("job", None),
+            value.get("religion", None),
+        ]
+        for i in range(1, 34):
+            if i in value.get("hobbyList", []):
+                hobby_value = 1
+            else:
+                hobby_value = 0
+            row_data.append(hobby_value)
+        data.append(row_data)
+
+    columnNames = ['memberId', 'ageGroup', 'gender', 'I/E', 'S/N', 'F/T', 'P/J', 'job', 'religion']
+    for i in range(1, 34):
+        newColumn = f'hobby{i}'
+        columnNames.append(newColumn)
+
+    mdf = pd.DataFrame(data, columns=columnNames)
+
+    return mdf
+
+def calculateSim(matrix):
+    x = matrix.iloc[:, 1:]
+    cosineMatrix = cosine_similarity(x, x)
+    cosineDf = pd.DataFrame(cosineMatrix, columns=matrix.iloc[:, 0], index=matrix.iloc[:, 0])
+    cosineDf.to_csv(filePath + 'userCosineSimilarity.csv')
+
+def findBest(A):
+    cosine_similarity_df = pd.DataFrame(cosine_similarity_matrix)
+    answer = cosine_similarity_df.sort_values(by=f'{A}', ascending=False)
+    return answer['memberId'].values.tolist()
