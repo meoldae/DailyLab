b4d1e2e30c57c7bff5b26ab5838858e4bcae1613
Junyoung <wnsdud12365@naver.com>
2023-09-26 12:57:40 +0900

    FIX: 하루 마무리 스케쥴러 로직 수정

--- backend/src/main/java/com/amor4ti/dailylab/global/schedule/ForceCompleteScheduler.java
+++ backend/src/main/java/com/amor4ti/dailylab/global/schedule/ForceCompleteScheduler.java
@@ -23,23 +23,31 @@ public class ForceCompleteScheduler {
 	private final TodoService todoService;
 	private final MemberService memberService;
 
-	@Scheduled(cron = "0 0 6 * * *")
+	@Scheduled(cron = "30 0 0 * * *")
 	@Transactional
 	public void forceCompleteDay() {
-		LocalDate today = LocalDate.now().minusDays(1);
-		List<Long> proceedMemberList = memberService.getProceedMemberList(today);
-
-		log.info("하루를 마무리 하지 않은 사용자가 {}명 있습니다.", proceedMemberList.size());
+		// 자정 ~ 새벽 06시 사이
+		LocalDate today1 = LocalDate.now().minusDays(1);
+		// 자정 이전
+		LocalDate today2 = LocalDate.now();
+		List<Long> proceedMember1List = memberService.getProceedMemberList(today1);
+		List<Long> proceedMember2List = memberService.getProceedMemberList(today2);
 
 		LocalDate tomorrow = LocalDate.now();
 
 		// 확정 일기 생성 및 내일 Todo 추천
-		proceedMemberList.stream().parallel().forEach(memberId -> {
+		proceedMember1List.stream().parallel().forEach(memberId -> {
 			diaryService.createConfirmDiary(memberId, tomorrow);
 			todoService.recommendTodo(memberId, tomorrow.toString());
 		});
 
-		log.info("하루를 마무리 하지 않은 사용자 {}명의 하루가 마무리 되었습니다.", proceedMemberList.size());
+		// 확정 일기 생성 및 내일 Todo 추천
+		proceedMember2List.stream().parallel().forEach(memberId -> {
+			diaryService.createConfirmDiary(memberId, tomorrow);
+			todoService.recommendTodo(memberId, tomorrow.toString());
+		});
+
+		log.info("하루를 마무리 하지 않은 사용자들 하루가 마무리 되었습니다.");
 	}
 
 }
