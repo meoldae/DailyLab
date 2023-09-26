4d1b6fd79aa04723e189a68fb7eb8a3281e433da
Junyoung <wnsdud12365@naver.com>
2023-09-26 16:52:50 +0900

    FEAT: 하루 요약하여 맛 반환

--- backend/src/main/java/com/amor4ti/dailylab/domain/emotion/service/EmotionServiceImpl.java
+++ backend/src/main/java/com/amor4ti/dailylab/domain/emotion/service/EmotionServiceImpl.java
@@ -1,17 +1,20 @@
 package com.amor4ti.dailylab.domain.emotion.service;
 
-
 import com.amor4ti.dailylab.domain.emotion.dto.request.RegisterMemberEmotionDto;
+import com.amor4ti.dailylab.domain.emotion.dto.response.EmotionCount;
 import com.amor4ti.dailylab.domain.emotion.dto.response.EmotionDetail;
 import com.amor4ti.dailylab.domain.emotion.dto.response.MemberEmotionDayDto;
 import com.amor4ti.dailylab.domain.emotion.dto.response.MemberEmotionPeriodDto;
+import com.amor4ti.dailylab.domain.emotion.dto.response.TasteVectorTable;
 import com.amor4ti.dailylab.domain.emotion.entity.MemberEmotion;
 import com.amor4ti.dailylab.domain.emotion.mongorepo.EmotionRepository;
 import com.amor4ti.dailylab.domain.emotion.entity.Emotion;
 import com.amor4ti.dailylab.domain.emotion.mongorepo.MemberEmotionRepository;
 import com.mongodb.BasicDBObject;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.bson.Document;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -19,7 +22,9 @@ import org.springframework.data.mongodb.core.aggregation.*;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -29,131 +34,166 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class EmotionServiceImpl implements EmotionService {
 
-    private final EmotionRepository emotionRepository;
-    private final MemberEmotionRepository memberEmotionRepository;
-    private final MongoTemplate mongoTemplate;
+	private final EmotionRepository emotionRepository;
+	private final MemberEmotionRepository memberEmotionRepository;
+	private final MongoTemplate mongoTemplate;
 
-    public List<Emotion> getAllEmotion() {
-        return emotionRepository.findAll();
-    }
+	public List<Emotion> getAllEmotion() {
+		return emotionRepository.findAll();
+	}
 
-    public void registerEmotion(RegisterMemberEmotionDto requestDto) {
-        String[] dates = requestDto.getTimeStamp().split(" ");
+	public void registerEmotion(RegisterMemberEmotionDto requestDto) {
+		String[] dates = requestDto.getTimeStamp().split(" ");
 
-        String date = dates[0];
-        String timestamp = dates[1];
+		String date = dates[0];
+		String timestamp = dates[1];
 
-        // 해당 memberId와 date에 해당하는 도큐먼트 조회
-        Optional<MemberEmotion> memberEmotionDay = memberEmotionRepository.findByMemberIdAndDate(requestDto.getMemberId(), date);
+		// 해당 memberId와 date에 해당하는 도큐먼트 조회
+		Optional<MemberEmotion> memberEmotionDay = memberEmotionRepository.findByMemberIdAndDate(
+			requestDto.getMemberId(), date);
 
-        if (!memberEmotionDay.isPresent()) {
-            // 도큐먼트가 없는 경우, 새로운 도큐먼트 생성
-            MemberEmotion newEmotionDay = MemberEmotion.build(requestDto.getMemberId(), date, new ArrayList<>());
+		if (!memberEmotionDay.isPresent()) {
+			// 도큐먼트가 없는 경우, 새로운 도큐먼트 생성
+			MemberEmotion newEmotionDay = MemberEmotion.build(requestDto.getMemberId(), date, new ArrayList<>());
 
-            newEmotionDay.getEmotions().add(new EmotionDetail(requestDto.getEmotionId(), timestamp));
-            memberEmotionRepository.save(newEmotionDay);
-        } else {
-            // 도큐먼트가 이미 있는 경우, emotions 배열에 추가
-            MemberEmotion memberEmotion = memberEmotionDay.get();
-            memberEmotion.getEmotions().add(new EmotionDetail(requestDto.getEmotionId(), timestamp));
-            memberEmotionRepository.save(memberEmotion);
-        }
-    }
+			newEmotionDay.getEmotions().add(new EmotionDetail(requestDto.getEmotionId(), timestamp));
+			memberEmotionRepository.save(newEmotionDay);
+		} else {
+			// 도큐먼트가 이미 있는 경우, emotions 배열에 추가
+			MemberEmotion memberEmotion = memberEmotionDay.get();
+			memberEmotion.getEmotions().add(new EmotionDetail(requestDto.getEmotionId(), timestamp));
+			memberEmotionRepository.save(memberEmotion);
+		}
+	}
 
-    public List<MemberEmotionDayDto> getDayEmotion(Long memberId, String date) {
-        Criteria memberCriteria = Criteria.where("memberId").is(memberId).and("date").is(date);
-        MatchOperation matchStage = Aggregation.match(memberCriteria);
+	public List<MemberEmotionDayDto> getDayEmotion(Long memberId, String date) {
+		Criteria memberCriteria = Criteria.where("memberId").is(memberId).and("date").is(date);
+		MatchOperation matchStage = Aggregation.match(memberCriteria);
 
-        LookupOperation lookupStage = Aggregation.lookup("emotion", "emotions.emotionId", "emotionId", "emotionInfo");
+		LookupOperation lookupStage = Aggregation.lookup("emotion", "emotions.emotionId", "emotionId", "emotionInfo");
 
-        Aggregation aggregation = Aggregation.newAggregation(matchStage, lookupStage);
+		Aggregation aggregation = Aggregation.newAggregation(matchStage, lookupStage);
 
-        List<Document> results = mongoTemplate.aggregate(aggregation, "member_emotion", Document.class).getMappedResults();
+		List<Document> results = mongoTemplate.aggregate(aggregation, "member_emotion", Document.class)
+			.getMappedResults();
 
-        return results.stream()
-                .flatMap(document -> {
-                    List<Document> emotions = (List<Document>) document.get("emotions");
-                    List<Document> emotionInfoList = (List<Document>) document.get("emotionInfo");
+		return results.stream()
+			.flatMap(document -> {
+				List<Document> emotions = (List<Document>)document.get("emotions");
+				List<Document> emotionInfoList = (List<Document>)document.get("emotionInfo");
 
-                    return emotions.stream().map(emotion -> {
-                        Integer emotionId = emotion.getInteger("emotionId");
-                        Document matchedEmotionInfo = emotionInfoList.stream()
-                                                                     .filter(e -> e.getInteger("emotionId").equals(emotionId))
-                                                                     .findFirst()
-                                                                     .orElse(null);
+				return emotions.stream().map(emotion -> {
+					Integer emotionId = emotion.getInteger("emotionId");
+					Document matchedEmotionInfo = emotionInfoList.stream()
+						.filter(e -> e.getInteger("emotionId").equals(emotionId))
+						.findFirst()
+						.orElse(null);
 
-                        String type = matchedEmotionInfo != null ? matchedEmotionInfo.getString("type") : null;
+					String type = matchedEmotionInfo != null ? matchedEmotionInfo.getString("type") : null;
 
-                        return MemberEmotionDayDto.builder()
-                                                  .emotionId(emotionId)
-                                                  .type(type)
-                                                  .timeStamp(emotion.getString("timestamp"))
-                                                  .build();
-                    });
-                })
-                .collect(Collectors.toList());
-    }
+					return MemberEmotionDayDto.builder()
+						.emotionId(emotionId)
+						.type(type)
+						.timeStamp(emotion.getString("timestamp"))
+						.build();
+				});
+			})
+			.collect(Collectors.toList());
+	}
 
-    public List<MemberEmotionPeriodDto> getEmotionsBetweenDates(Long memberId, String startDate, String endDate) {
-        // 1. memberId와 date 범위를 기준으로 필터링
-        MatchOperation matchOperation = Aggregation.match(
-                Criteria.where("memberId").is(memberId)
-                        .andOperator(
-                                Criteria.where("date").gte(startDate),
-                                Criteria.where("date").lte(endDate)
-                        )
-        );
+	public List<MemberEmotionPeriodDto> getEmotionsBetweenDates(Long memberId, String startDate, String endDate) {
+		// 1. memberId와 date 범위를 기준으로 필터링
+		MatchOperation matchOperation = Aggregation.match(
+			Criteria.where("memberId").is(memberId)
+				.andOperator(
+					Criteria.where("date").gte(startDate),
+					Criteria.where("date").lte(endDate)
+				)
+		);
 
-        // 2. Unwind operation to flatten the emotions array
-        UnwindOperation unwindOperation = Aggregation.unwind("emotions");
+		// 2. Unwind operation to flatten the emotions array
+		UnwindOperation unwindOperation = Aggregation.unwind("emotions");
 
-        // 3. emotion 컬렉션과 조인
-        LookupOperation lookupOperation = LookupOperation.newLookup()
-                                                         .from("emotion")
-                                                         .localField("emotions.emotionId")
-                                                         .foreignField("emotionId")
-                                                         .as("emotionInfo");
+		// 3. emotion 컬렉션과 조인
+		LookupOperation lookupOperation = LookupOperation.newLookup()
+			.from("emotion")
+			.localField("emotions.emotionId")
+			.foreignField("emotionId")
+			.as("emotionInfo");
 
-        // 3.5 Unwind operation to flatten the emotionInfo array
-        UnwindOperation unwindOperation2 = Aggregation.unwind("emotionInfo");
+		// 3.5 Unwind operation to flatten the emotionInfo array
+		UnwindOperation unwindOperation2 = Aggregation.unwind("emotionInfo");
 
-        // 4. 조인된 결과와 함께 각 날짜 및 감정별로 그룹화
-        GroupOperation firstGroupOperation = Aggregation.group("date", "emotions.emotionId")
-                                                        .first("emotionInfo.type")
-                                                        .as("type")
-                                                        .count().as("count");
+		// 4. 조인된 결과와 함께 각 날짜 및 감정별로 그룹화
+		GroupOperation firstGroupOperation = Aggregation.group("date", "emotions.emotionId")
+			.first("emotionInfo.type")
+			.as("type")
+			.count().as("count");
 
-        // 5. 각 날짜별로 재그룹화하며 감정 정보 포함
-        GroupOperation secondGroupOperation = Aggregation.group("_id.date")
-                                                         .push(new BasicDBObject("emotionId", "$_id.emotionId")
-                                                                 .append("count", "$count")
-                                                                 .append("type", "$type"))
-                                                         .as("emotions");
+		// 5. 각 날짜별로 재그룹화하며 감정 정보 포함
+		GroupOperation secondGroupOperation = Aggregation.group("_id.date")
+			.push(new BasicDBObject("emotionId", "$_id.emotionId")
+				.append("count", "$count")
+				.append("type", "$type"))
+			.as("emotions");
 
-        // 6. 날짜 필드를 직접 설정
-        ProjectionOperation projectionOperation = Aggregation.project()
-                                                             .andExpression("_id").as("date")
-                                                             .and("emotions").as("emotions");
+		// 6. 날짜 필드를 직접 설정
+		ProjectionOperation projectionOperation = Aggregation.project()
+			.andExpression("_id").as("date")
+			.and("emotions").as("emotions");
 
-        // 7. 날짜별로 정렬
-        SortOperation sortOperation = Aggregation.sort(Sort.by(Sort.Order.asc("date")));
+		// 7. 날짜별로 정렬
+		SortOperation sortOperation = Aggregation.sort(Sort.by(Sort.Order.asc("date")));
 
-        Aggregation aggregation = Aggregation.newAggregation(
-                                                matchOperation,
-                                                unwindOperation,
-                                                lookupOperation,
-                                                unwindOperation2,
-                                                firstGroupOperation,
-                                                secondGroupOperation,
-                                                projectionOperation,
-                                                sortOperation
-        );
+		Aggregation aggregation = Aggregation.newAggregation(
+			matchOperation,
+			unwindOperation,
+			lookupOperation,
+			unwindOperation2,
+			firstGroupOperation,
+			secondGroupOperation,
+			projectionOperation,
+			sortOperation
+		);
 
-        // 8. 집계 연산 수행
-        AggregationResults<MemberEmotionPeriodDto> results = mongoTemplate.aggregate(
-                aggregation, "member_emotion", MemberEmotionPeriodDto.class
-        );
+		// 8. 집계 연산 수행
+		AggregationResults<MemberEmotionPeriodDto> results = mongoTemplate.aggregate(
+			aggregation, "member_emotion", MemberEmotionPeriodDto.class
+		);
 
-        return results.getMappedResults();
-    }
+		return results.getMappedResults();
+	}
+
+	@Override
+	public String getSelectTaste(Long memberId, LocalDate now) {
+		List<MemberEmotionPeriodDto> queryResult = getEmotionsBetweenDates(memberId, now.toString(),
+			now.toString());
+
+		int[] result = new int[15];
+
+		MemberEmotionPeriodDto data = queryResult.get(0);
+		// TODO 통계를 위한 날짜
+        LocalDate date = LocalDate.parse(data.getDate());
+
+		List<EmotionCount> emotions = data.getEmotions();
+		emotions.stream().forEach(emotion -> {
+				int emotionId = Integer.parseInt(emotion.getEmotionId()) - 1;
+				int count = emotion.getCount().intValue();
+
+				for (int i = 0; i < TasteVectorTable.tasteVectorTable[emotionId].length; i++) {
+                    result[i] += TasteVectorTable.tasteVectorTable[emotionId][i] * count;
+				}
+			}
+		);
+		int maxValue = 0;
+		int tasteIndex = 0;
+		for (int i = result.length - 1; i >= 0; i--) {
+			if (result[i] > maxValue) {
+				maxValue = result[i];
+				tasteIndex = i;
+			}
+
+		}
+		return TasteVectorTable.tasteList[tasteIndex];
+	}
 }
