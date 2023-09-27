21b23d69099d16dd08f3908fa7edc525f48aa222
김주하 <tkfkd122@naver.com>
2023-09-27 15:34:35 +0900

    FEAT: 사용자 유사도 평가 추천 리스트 dto 생성

---
+++ backend/src/main/java/com/amor4ti/dailylab/domain/todo/dto/response/OtherTodoDto.java
@@ -0,0 +1,13 @@
+package com.amor4ti.dailylab.domain.todo.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class OtherTodoDto {
+        private String small;
+        private Integer count;
+}
