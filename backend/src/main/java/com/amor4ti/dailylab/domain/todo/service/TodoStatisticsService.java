9a94469ad70eaad03325386fb65cf1bd08d42f95
김주하 <tkfkd122@naver.com>
2023-09-28 23:00:41 +0900

    FEAT: 투두 통계 개인, 그룹, 전체 get 기능 추가

--- backend/src/main/java/com/amor4ti/dailylab/domain/todo/service/TodoStatisticsService.java
+++ backend/src/main/java/com/amor4ti/dailylab/domain/todo/service/TodoStatisticsService.java
@@ -8,5 +8,7 @@ import java.time.LocalDate;
 public interface TodoStatisticsService {
     Object connection(Object data, String url);
     DataResponse getOtherList(Long memberId);
-    DataResponse getTodoSummary(Long memberId, LocalDate startDate, LocalDate endDate);
+    DataResponse getGroupTodoSummary(Long memberId, LocalDate startDate, LocalDate endDate);
+    DataResponse getPersonalTodoSummary(Long memberId, LocalDate startDate, LocalDate endDate);
+    DataResponse getAllTodoSummary(Long memberId, LocalDate startDate, LocalDate endDate);
 }
