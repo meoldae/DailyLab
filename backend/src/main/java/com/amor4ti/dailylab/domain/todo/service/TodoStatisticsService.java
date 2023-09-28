384e7e82f672a105d8301ba4220f0c6f0ddad257
김주하 <tkfkd122@naver.com>
2023-09-28 13:51:28 +0900

    FEAT: 연령대, 성별 구분 함수 추가

--- backend/src/main/java/com/amor4ti/dailylab/domain/todo/service/TodoStatisticsService.java
+++ backend/src/main/java/com/amor4ti/dailylab/domain/todo/service/TodoStatisticsService.java
@@ -3,7 +3,10 @@ package com.amor4ti.dailylab.domain.todo.service;
 import com.amor4ti.dailylab.global.response.CommonResponse;
 import com.amor4ti.dailylab.global.response.DataResponse;
 
-public interface TodoStasticsService {
+import java.time.LocalDate;
+
+public interface TodoStatisticsService {
     Object connection(Object data, String url);
     DataResponse getOtherList(Long memberId);
+    DataResponse getTodoSummary(Long memberId, LocalDate startDate, LocalDate endDate);
 }
--- backend/src/main/java/com/amor4ti/dailylab/domain/todo/service/TodoStatisticsServiceImpl.java
+++ backend/src/main/java/com/amor4ti/dailylab/domain/todo/service/TodoStatisticsServiceImpl.java
@@ -1,12 +1,11 @@
 package com.amor4ti.dailylab.domain.todo.service;
 
 import com.amor4ti.dailylab.domain.category.service.CategoryService;
+import com.amor4ti.dailylab.domain.entity.Member;
 import com.amor4ti.dailylab.domain.member.repository.MemberRepository;
 import com.amor4ti.dailylab.domain.todo.dto.response.OtherTodoDto;
-import com.amor4ti.dailylab.domain.todo.repository.TodoRepository;
 import com.amor4ti.dailylab.global.exception.CustomException;
 import com.amor4ti.dailylab.global.exception.ExceptionStatus;
-import com.amor4ti.dailylab.global.response.CommonResponse;
 import com.amor4ti.dailylab.global.response.DataResponse;
 import com.amor4ti.dailylab.global.response.ResponseService;
 import com.amor4ti.dailylab.global.response.ResponseStatus;
@@ -15,20 +14,21 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
 
+import java.time.LocalDate;
 import java.util.*;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class TodoStasticsServiceImpl implements  TodoStasticsService{
+public class TodoStatisticsServiceImpl implements TodoStatisticsService {
 
     @Value("${data-server-url}")
     private String DATA_SERVER_URL;
     private final WebClientUtil webClientUtil;
     private final ResponseService responseService;
     private final CategoryService categoryService;
+    private final MemberRepository memberRepository;
 
     @Override
     public DataResponse getOtherList(Long memberId) {
@@ -51,6 +51,18 @@ public class TodoStasticsServiceImpl implements  TodoStasticsService{
         }
 
         return responseService.successDataResponse(ResponseStatus.RESPONSE_SUCCESS, list);
+    }
+
+    @Override
+    public DataResponse getTodoSummary(Long memberId, LocalDate startDate, LocalDate endDate) {
+        Member findMember = memberRepository.findById(memberId)
+                .orElseThrow(() -> new CustomException(ExceptionStatus.MEMBER_NOT_FOUND));
+
+        int memberAge = (LocalDate.now().getYear() - findMember.getBirthday().getYear())/10;
+        String gender = findMember.getGender();
+
+
+        return null;
     }
 
     @Override
