da62888a6a7c0dee8f8065be7e2a7478d2b021a6
JM <k3371548@gmail.com>
2023-10-01 01:29:10 +0900

    FIX: 1001 JM commit

--- frontend/src/api/Interceptor.ts
+++ frontend/src/api/Interceptor.ts
@@ -24,7 +24,7 @@ const onResponse = (res: AxiosResponse): AxiosResponse => {
 }
 
 /* http response가 catch로 넘어가기 전에 호출되는 함수이다.*/
-const onErrorResponse = async (err: AxiosError | Error): Promise<AxiosError> => {
+const onErrorResponse = async (err: AxiosError | Error) => {
   const _err = err as unknown as AxiosError; // err 객체의 타입은 unknown이므로 타입 단언을 해주어야 한다
   const { response } = _err; // err 객체에서 response 를 구조 분해 할당
   const originalConfig = _err.config as InternalAxiosRequestConfig; // 기존의 요청 정보를 저장한다.
@@ -37,9 +37,7 @@ const onErrorResponse = async (err: AxiosError | Error): Promise<AxiosError> => 
       
       return axios.request(originalConfig);
     }, (error) => console.log(error));
-  }
-
-  return Promise.reject(err);
+  } else return Promise.reject(err);
 };
 
 export {onRequest, onErrorRequest, onResponse, onErrorResponse};
