34909e50fcc26948f96790fe72654c76df386f13
Jiyoung0913 <xkagja2006@naver.com>
2023-10-04 21:37:13 +0900

    FEAT : 1004 JY 재커밋

--- frontend/src/components/tutorial/tutorialItem/TodoTutorial.tsx
+++ frontend/src/components/tutorial/tutorialItem/TodoTutorial.tsx
@@ -15,7 +15,7 @@ const TodoTutorial = () => {
             {/* 설명 부분 */}
             <div className="flex text-center items-center -mt-[30px]">
                 <img className="w-[120px]" src={ian2Img} alt="" />
-                <span className="mt-[20px] text-2xl !font-light">오늘의 할 일을 추천해드려요!<br/>추천 항목에는 <span className="text-orange">테두리</span>가 있어요<br/>왼쪽으로 당겨서 삭제하거나<br/>관심없음을 설정할 수 있어요</span>
+                <span className="mt-[30px] text-2xl !font-light">오늘의 할 일을 추천해드려요!<br/>추천 항목에는 <span className="text-orange">테두리</span>가 있어요<br/>왼쪽으로 당겨서 삭제하거나<br/>관심없음을 설정할 수 있어요</span>
             </div>
         </>
     )
