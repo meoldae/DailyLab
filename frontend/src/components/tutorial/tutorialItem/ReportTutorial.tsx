29a91118c9d5720d1f703c32a613bf8ca7e1d451
Jiyoung0913 <xkagja2006@naver.com>
2023-10-05 12:57:06 +0900

    FEAT : 2005 JY 튜토리얼 페이지 이미지 수정, 글씨 수정

--- frontend/src/components/tutorial/tutorialItem/ReportTutorial.tsx
+++ frontend/src/components/tutorial/tutorialItem/ReportTutorial.tsx
@@ -7,13 +7,13 @@ const ReportTutorial = () => {
             <div className="text-center p-[20px] font-semibold text-[24px] mt-[20px]">
                 <span className="text-blue">연구 보고서</span>받기
             </div>
-            <div className="bg_contents_con p-[20px] h-[50vh] pt-[30px] overflow-hidden flex justify-center"> 
-                <img className="m-auto w-[300px]" src={GetMode() === 'dark' ? "./assets/img/tutorial/report_dark.png" : "./assets/img/tutorial/report_light.png"} alt="" />
+            <div className="bg_contents_con p-[20px] h-[50vh] flex items-center">
+                <img className="w-full" src={GetMode() === 'dark' ? "./assets/img/tutorial/report_dark.png" : "./assets/img/tutorial/report_light.png"} alt="" />
             </div>
             {/* 설명 부분 */}
             <div className="flex text-center items-center">
                 <img className="w-[130px] -mt-[30px]" src={diego2Img} alt="" />
-                <span className="mt-[20px] text-2xl !font-light"><span className="text-blue">하루 마무리</span> 버튼을 누르면<br/>일정 시간이 지난 후<br/>연구 보고서를 확인할 수 있어요!</span>
+                <span className="mt-[20px] text-2xl !font-semibold"><span className="text-blue !font-black">하루 마무리</span> 버튼을 누르면<br/>일정 시간이 지난 후<br/>연구 보고서를 확인할 수 있어요!</span>
             </div>
         </>
     )
--- frontend/src/components/tutorial/tutorialItem/ScheduleTutorial.tsx
+++ frontend/src/components/tutorial/tutorialItem/ScheduleTutorial.tsx
@@ -7,13 +7,13 @@ const ScheduleTutorial = () => {
             <div className="text-center p-[20px] font-semibold text-[24px] mt-[20px]">
                 <span className="text-red">나의 기록 </span>돌아보기
             </div>
-            <div className="bg_contents_con p-[20px] h-[50vh] pt-[30px]  flex justify-center">
-                <img className="m-auto w-[250px]" src={GetMode() === 'dark' ? "./assets/img/tutorial/schedule_dark.png" : "./assets/img/tutorial/schedule_light.png"} alt="" />
+            <div className="bg_contents_con p-[20px] h-[50vh] flex items-center">
+                <img className="w-full" src={GetMode() === 'dark' ? "./assets/img/tutorial/schedule_dark.png" : "./assets/img/tutorial/schedule_light.png"} alt="" />
             </div>
             {/* 설명 부분 */}
             <div className="flex text-center items-center">
                 <img className="w-[150px] -mt-[20px] -mr-[30px]" src={cocoImg} alt="" />
-                <span className="-mt-[20px] text-2xl !font-light">하단의 달력 아이콘을 눌러<br/>이전의 보고서를 확인하고,<br/>미래의 할 일을 미리 입력할 수 있어요</span>
+                <span className="-mt-[20px] text-2xl !font-semibold">하단의 달력 아이콘을 눌러<br/>이전의 보고서를 확인하고,<br/>미래의 할 일을 미리 입력할 수 있어요</span>
             </div>
         </>
     )
