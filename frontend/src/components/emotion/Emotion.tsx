cfd7f4c9cf8dbfba7c0037f93c0e77cef69fdeb6
Jiyoung0913 <xkagja2006@naver.com>
2023-10-05 21:42:08 +0900

    FIX : 1005 JY

--- frontend/src/components/emotion/Emotion.tsx
+++ frontend/src/components/emotion/Emotion.tsx
@@ -54,35 +54,37 @@ const Emotion = (props: props) => {
     };
   
     return (
-      <div className="pb-[70px] px-[30px] pt-[20px]">
-        <div onClick={props.changeMode} className="mb-[30px] inline-flex items-center">
-            <img className="w-[30px] transform scale-x-[-1]" src="./assets/img/icon/arrow_right.png" alt="" />
-            <p className="text-2xl font-semibold">돌아가기</p>
-        </div>
-        <div className="mb-[40px] flex justify-center items-center py-[20px] bg-primary rounded-3xl text-2xl font-light text-center min-h-[80px]">
-            {circleCount === 0
-                ? (<div>오늘은 어떤 기분이신가요?</div>)
-                : (<div>이 감정은... <p className="inline font-bold" >{emotionName}!</p><br/> {emotionType === 'n' ? "안좋은 감정은 제가 대신 가져갈게요" : "저까지 기분이 좋아지네요"}</div>)
-            }
-        </div>
-        <motion.div animate={controls} className="relative mb-[30px]">
-            <img className="w-[150px] mx-auto" src={cloeNoFaceImg} alt="클로에" />
-            <img className="absolute top-[20px] left-[calc(50%-50px)] w-[100px]" src={circleCount === 0 ? `./assets/img/emotion/face/100.png` : `./assets/img/emotion/face/${emotionNo}.png`} alt="표정" />
-        </motion.div>
-        <div className="overflow-y-auto max-h-[300px] bg_contents_con p-[20px] w-full flex flex-wrap items-center justify-center">
-            {emotionList.length > 0 && emotionList.map((emo, index) => (
-                <div className="text-center w-[20%]">
-                    <button className="p-1 transition-transform duration-300 ease-in-out active:scale-125 webkit-tap-highlight-color" key={index} onClick={() => {onEmotionClick(emo.emotionId, emo.name, emo.type)}}>
-                        <img className="w-[40px]" src={`./assets/img/emotion/${emo.emotionId}.png`} alt={emo.name} />
-                        <p className="font-light text-xl p-2 text-text">{emo.name}</p>
-                    </button>
+        <div className="h-[100vh]">
+            <div className="px-[30px] pt-[20px]">
+                <div onClick={props.changeMode} className="mb-[30px] inline-flex items-center">
+                    <img className="w-[30px] transform scale-x-[-1]" src="./assets/img/icon/arrow_right.png" alt="" />
+                    <p className="text-2xl font-semibold">돌아가기</p>
                 </div>
-            ))}
+                <div className="mb-[40px] flex justify-center items-center py-[20px] bg-primary rounded-3xl text-2xl font-light text-center min-h-[80px]">
+                    {circleCount === 0
+                        ? (<div>오늘은 어떤 기분이신가요?</div>)
+                        : (<div>이 감정은... <p className="inline font-bold" >{emotionName}!</p><br/> {emotionType === 'n' ? "안좋은 감정은 제가 대신 가져갈게요" : "저까지 기분이 좋아지네요"}</div>)
+                    }
+                </div>
+                <motion.div animate={controls} className="relative mb-[30px]">
+                    <img className="w-[150px] mx-auto" src={cloeNoFaceImg} alt="클로에" />
+                    <img className="absolute top-[20px] left-[calc(50%-50px)] w-[100px]" src={circleCount === 0 ? `./assets/img/emotion/face/100.png` : `./assets/img/emotion/face/${emotionNo}.png`} alt="표정" />
+                </motion.div>
+                <div className="overflow-y-auto max-h-[300px] bg_contents_con p-[20px] w-full flex flex-wrap items-center justify-center">
+                    {emotionList.length > 0 && emotionList.map((emo, index) => (
+                        <div className="text-center w-[20%]">
+                            <button className="p-1 transition-transform duration-300 ease-in-out active:scale-125 webkit-tap-highlight-color" key={index} onClick={() => {onEmotionClick(emo.emotionId, emo.name, emo.type)}}>
+                                <img className="w-[40px]" src={`./assets/img/emotion/${emo.emotionId}.png`} alt={emo.name} />
+                                <p className="font-light text-xl p-2 text-text">{emo.name}</p>
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div id="matterCanvasCon" className="mt-[20px] absolute left-0 top-0 z-[-1] child-[canvas]:max-h-full">
+                <EmotionMatter circleCount={circleCount} emotionNo={emotionNo} />
+            </div>
         </div>
-        <div id="matterCanvasCon" className="absolute left-0 top-0 z-[-1] child-[canvas]:max-h-full">
-            <EmotionMatter circleCount={circleCount} emotionNo={emotionNo} />
-        </div>
-      </div>
     );
   };
   
